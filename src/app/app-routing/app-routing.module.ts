fa755caf52882afe17d085cd806f24a22c797ec4
madhavaxisrooms <madhav.sharma@axisrooms.com>
2018-05-04 15:26:26 +0530

    Dashboard module added && home component created

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -5,9 +5,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from '../shared/components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
+  { path: '', redirectTo:'dashboard',pathMatch: 'full' },
   { path: 'billing', loadChildren: 'app/billing/billing.module#BillingModule' },
   { path: 'activity-report', loadChildren: 'app/activity-report/activity-report.module#ActivityReportModule' },
   { path: 'analytics', loadChildren: 'app/analytics/analytics.module#AnalyticsModule' },
+  { path: 'dashboard', loadChildren: 'app/dashboard/dashboard.module#DashboardModule' },
   // { path: '', redirectTo: '/support/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
   { path: '404', component: PageNotFoundComponent },
