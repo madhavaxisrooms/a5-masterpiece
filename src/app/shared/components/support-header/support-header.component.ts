0536cba302736f7a9480940551633c98a919ac30
madhavaxisrooms <madhav.sharma@axisrooms.com>
2018-05-16 13:36:43 +0530

    Header Changes

--- src/app/shared/components/support-header/support-header.component.ts
+++ src/app/shared/components/support-header/support-header.component.ts
@@ -12,14 +12,15 @@ export class SupportHeaderComponent implements OnInit {
   public levelOne = [];
   public lowerLevels = [];
 
+  public dashboardVisible:boolean = false;
+
   constructor(
-    private headerService: HeaderService
+    private headerService: HeaderService,
   ) { }
 
   ngOnInit() {
     this.header = this.headerService.headerJson;
-    this.traverse(this.header);
-
+    
     // this.headerService.getHeaderData().subscribe(
     //   res => {
     //     this.header = res;
@@ -29,64 +30,11 @@ export class SupportHeaderComponent implements OnInit {
     //     console.log(err);
     //   }
     // );
+
+    this.headerService.getSupplierViewHeaderData(262);
+
+    this.header.menus.splice(-1,1);
   }
 
-  traverse(jsonObj) {
-    for (let i = 0; i < jsonObj.menus.length; i++) {
-
-      let l1 = {
-        name: jsonObj.menus[i].name,
-        url: jsonObj.menus[i].actionUrl,
-        trigger: null
-      };
-      this.levelOne.push(l1);
-      //Second Level
-      if (jsonObj.menus[i].submenus) {
-        l1.trigger = jsonObj.menus[i].name;
-        let l2 = {
-          tRef: {
-            tRef: jsonObj.menus[i].name
-          },
-          menuOptions: []
-        }
-        for (let j = 0; j < jsonObj.menus[i].submenus.length; j++) {
-
-          let l2Option = {
-            name: jsonObj.menus[i].submenus[j].name,
-            url: jsonObj.menus[i].submenus[j].actionUrl,
-            trigger: null
-          }
-          l2.menuOptions.push(l2Option)
-
-          if (j == jsonObj.menus[i].submenus.length - 1) {
-            this.lowerLevels.push(l2);
-          }
-
-          //Third Level
-          if (jsonObj.menus[i].submenus[j].submenus) {
-            let l3 = {
-              tRef: {
-                tRef: jsonObj.menus[i].name
-              },
-              menuOptions: []
-            }
-
-            for (let k = 0; k < jsonObj.menus[i].submenus[j].submenus.length; k++) {
-              let l3Option = {
-                name: jsonObj.menus[i].submenus[j].submenus[k].name,
-                url: jsonObj.menus[i].submenus[j].submenus[k].actionUrl,
-                trigger: null
-              };
-              l3.menuOptions.push(l3Option);
-              if (k == jsonObj.menus[i].submenus[j].submenus.length - 1) {
-                this.lowerLevels.push(l3);
-              }
-            }
-          }
-        }
-      }
-    }
-    console.log(this.levelOne);
-    console.log(this.lowerLevels);
-  }
+  
 }
