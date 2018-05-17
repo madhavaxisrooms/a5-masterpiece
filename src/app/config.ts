18cbb45e72472b50bee49dcc904e9dedd1e56582
Vijay Ramesh Kumar <vijay.ramesh@axisrooms.com>
2018-05-17 11:44:28 +0530

    git config

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,5 +1,5 @@
 export const config = {
   analytics_ip: 'http://94.130.54.42:58603/v1',
-  server_path: 'https://app.axisrooms.com',
+  server_path: 'https://support5.axisrooms.com',
   cm_url: 'http://support5.axisrooms.com/support/masterReport.html',
 };
